155e5269a3d56e39c6f7195815d2f31972e36214
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-02-24 13:34:28 -0800

    Add files via upload

---
+++ GameScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health cc = new Health();
+
+        Debug.Log(cc.health + "/" + cc.maxHealth);
+        Debug.Log("Health " + cc.CurrentHealthPercent() + "%");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Health.cs
@@ -0,0 +1,11 @@
+﻿public class Health
+{
+    public int health = 15;
+    public int maxHealth = 100;
+
+    public float CurrentHealthPercent()
+    {
+        return (float)health/maxHealth * 100;
+    }
+
+}
